8d6d50402ca70cc62a64c29f715cad9b49c6a593
RoboCode9 <ingresshelps@gmail.com>
2024-01-25 15:04:52 -0600

    First commit

---
+++ python tama/main.py
@@ -0,0 +1,30 @@
+# Pet Program
+
+from random import randrange
+
+class Pet (object):
+    """A VPet"""
+    excitement_reduce = 3
+    excitement_max = 10
+    excitement_warning = 3
+    food_reduce = 2
+    food_max = 10
+    food_warning = 3
+    vocab = ['"Arr..."', '"Hello"']
+
+    def __init__(self, name, animal_type):
+        self.name = name
+        self.animal_type = animal_type
+        self.food = randrange(self.food_max)
+        self.excitement = randrange(self.excitement_max)
+        self.vocab = self.vocab[:]
+
+    def __clock__tick(self):
+        self.excitement -= 1
+        self.food -= 1
+
+    @property
+    def mood(self):
+        if self.food > self.food_warning and self.excitement > self.excitement_warning:
+            return "happy"
+        
