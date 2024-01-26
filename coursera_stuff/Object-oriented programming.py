c3836cd88675b623578632ff9f4b4478c91789c5
RoboCode9 <ingresshelps@gmail.com>
2024-01-26 15:14:20 -0600

    more sample codes...

---
+++ coursera_stuff/Object-oriented programming.py
@@ -0,0 +1,8 @@
+magic = 'hocus pocus'
+print(type(magic))
+magic = magic.swapcase()
+print(magic)
+magic = magic.replace('CUS', 'KEY') #case sensitive!
+print(magic)
+magic = magic.split() 
+print(magic)
