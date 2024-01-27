d37bdc48340dfafd627ee745fbf9cd9697d41de7
RoboCode9 <ingresshelps@gmail.com>
2024-01-27 10:48:43 -0600

    min and max demonstration

---
+++ coursera_stuff/Variables and data types.py
@@ -0,0 +1,10 @@
+age_list = [34, 25, 23, 19, 29]
+max_age = max(age_list) #gets the highest number in this list
+print(max_age)
+max_age = str(max_age)
+print(max_age)
+max_age = 'ninety-nine'
+print(max_age)
+min_age = min(age_list)
+max_age = max(age_list)
+print(max_age - min_age)
