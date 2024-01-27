e46c4b8af2ddf9d7f09a97a78b6039ef9894614a
RoboCode9 <ingresshelps@gmail.com>
2024-01-27 11:41:23 -0600

    added a few comments to describe the code

--- coursera_stuff/Data types and conversions.py
+++ coursera_stuff/Data types and conversions.py
@@ -1,7 +1,8 @@
+# detecting data types
 print(type("A"))
 print(type(1))
 print(type(1.1))
 print(type(False))
 
-print(1 + 2.5)
+print(1 + 2.5) # implicitly converting to float
 print('2 + 2 = ' + str(2 + 2)) # explicitly converting 2 plus 2 to string
