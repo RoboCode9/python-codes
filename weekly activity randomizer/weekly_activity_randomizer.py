ed864b5d5797900081cafa0ecf1431a1540b4b92
RoboCode9 <ingresshelps@gmail.com>
2024-02-11 15:18:01 -0600

    added some randomness

--- weekly activity randomizer/weekly_activity_randomizer.py
+++ weekly activity randomizer/weekly_activity_randomizer.py
@@ -2,10 +2,18 @@ import random
 
 days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"] #create list of days
 activity = [] #for activities
+color = ['Red', 'Red', 'Red', 'Blue'] # create a list of colors.
+random.shuffle(color)
 
 for i in range(7): #iterate over a range of 7
     task = input(f'Enter activity #{i + 1}: ') #gather user input then add it to task variable
     activity.append(task) #append it to activity list
+
+random_color = random.choice(color)#randomly select from the color list
+
+if random_color == 'Blue': #if the selected color is 'Blue'
+    random_index = random.choice(range(len(activity))) #randomly select from the activity list and add that index value to random_index variable
+    activity[random_index] = random_color #replace selected activity index caused by the random_index with the random color, which should be blue
 
 random.shuffle(activity) #shuffle the activity list
 print("") #print blank line for better view
