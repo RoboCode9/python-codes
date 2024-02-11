4de8c7268590cad7d15b0d8467aace2b3ba2e435
RoboCode9 <ingresshelps@gmail.com>
2024-02-11 15:49:05 -0600

    made it menu driven

--- weekly activity randomizer/weekly_activity_randomizer.py
+++ weekly activity randomizer/weekly_activity_randomizer.py
@@ -5,20 +5,29 @@ activity = [] #for activities
 color = ['Red', 'Red', 'Red', 'Blue'] # create a list of colors.
 random.shuffle(color)
 
-for i in range(5): #iterate over a range of 7
-    task = input(f'Enter activity #{i + 1}: ') #gather user input then add it to task variable
-    activity.append(task) #append it to activity list
+while True:
+    selection = input('Would you like to run this weekly randomizer activity program? (yes or no) ')
 
-random_color = random.choice(color)#randomly select from the color list
+    if selection == 'yes':
+        for i in range(5): #iterate over a range of 7
+            task = input(f'Enter activity #{i + 1}: ') #gather user input then add it to task variable
+            activity.append(task) #append it to activity list
 
-if random_color == 'Blue': #if the selected color is 'Blue'
-    random_index = random.choice(range(len(activity))) #randomly select from the activity list and add that index value to random_index variable
-    activity[random_index] = random_color #replace selected activity index caused by the random_index with the random color, which should be blue
+        random_color = random.choice(color)#randomly select from the color list
 
-random.shuffle(activity) #shuffle the activity list
-print("") #print blank line for better view
+        if random_color == 'Blue': #if the selected color is 'Blue'
+            random_index = random.choice(range(len(activity))) #randomly select from the activity list and add that index value to random_index variable
+            activity[random_index] = random_color #replace selected activity index caused by the random_index with the random color, which should be blue
 
-for day, task in zip(days_of_week, activity): #iterate over both day_of_week and activity lists, if i only used "task" it would look like this: ('sunday', 'jump') etc.
-    print(f'This {day} you will: {task}')
+        random.shuffle(activity) #shuffle the activity list
+        print("") #print blank line for better view
 
-input("Press any key to continue...")
+        for day, task in zip(days_of_week, activity): #iterate over both day_of_week and activity lists, if i only used "task" it would look like this: ('sunday', 'jump') etc.
+            print(f'This {day} you will: {task}')
+
+    elif selection == 'no':
+        input("Press any key to continue...")
+        break
+
+    else:
+        print('invalid choice')
