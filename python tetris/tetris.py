de197affa2393034299cef30f72b51a92426bac4
RoboCode9 <ingresshelps@gmail.com>
2024-03-06 16:40:33 -0600

    grid complete

--- python tetris/tetris.py
+++ python tetris/tetris.py
@@ -1,12 +1,21 @@
 import pygame,sys
+from grid import Grid
 
 pygame.init()
-dark_blue = (44, 44, 127)
+dark_red = (127, 44, 44)
 
 screen = pygame.display.set_mode((300, 600))
-pygame.display.set_caption("Python Tetris")
+pygame.display.set_caption("Tetris")
 
 clock = pygame.time.Clock()
+
+game_grid = Grid()
+
+game_grid.grid[0][0] = 1
+game_grid.grid[3][5] = 4
+game_grid.grid[17][8] = 7
+
+game_grid.print_grid()
 
 while True:
     for event in pygame.event.get():
@@ -14,6 +23,8 @@ while True:
             pygame.quit()
             sys.exit()
     #Drawing
-    screen.fill(dark_blue)
+    screen.fill(dark_red)
+    game_grid.draw(screen)
+
     pygame.display.update()
     clock.tick(60)
